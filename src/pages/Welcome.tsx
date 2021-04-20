0a7c388bd370c7885b469af74612ef1448148648
Valdir Silva <valdir.ti@gmail.com>
2021-04-20 20:41:55 -0300

    feat: create and style Welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,12 +1,68 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {SafeAreaView, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
+
+import colors from '../styles/colors'
+
+import wateringImage from '../assets/watering.png'
 
 export function Welcome () {
     return (
-        <>
-            <View>
-                <Text>Welcome</Text>
-            </View>
-        </>
+        <SafeAreaView style={styles.container}>
+            
+            <Text style={styles.title}>Gerencie {'\n'}
+                suas plantas de {'\n'}
+                forma fácil
+            </Text>
+
+            <Image source={wateringImage} style={styles.image} />
+
+            <Text style={styles.subTitle}>
+                Não esqueça mais de regar suas {'\n'} 
+                plantas. Nós cuidamos de lembrar você {'\n'}
+                sempre que precisar.
+            </Text>
+
+            <TouchableOpacity style={styles.button} activeOpacity={0.75}>
+                <Text style={styles.buttonTitle}>></Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    image: {
+        width: 292,
+        height: 284
+    },
+    title: {
+        fontSize: 32,
+        color: colors.heading,
+        fontWeight: 'bold',
+        marginTop: 38,
+        textAlign:'center'
+    },
+    subTitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    button: {
+        width: 56,
+        height: 56,
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16
+    },
+    buttonTitle: {
+        color: colors.white,
+        fontSize: 16
+    }
+})
