6887c9d2b4b13abf955470307dfb46c9cd5f5923
Valdir Silva <valdir.ti@gmail.com>
2021-04-20 19:53:22 -0300

    feat: first commit changed images

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export function Welcome () {
+    return (
+        <>
+            <View>
+                <Text>Welcome</Text>
+            </View>
+        </>
+    )
+}
