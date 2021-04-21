ccd7cd4ade7db768e0f2dddc015d9ba54d3a5e89
Valdir Silva <valdir.ti@gmail.com>
2021-04-21 13:30:52 -0300

    feat: add Header component

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -5,12 +5,21 @@ import {
     StyleSheet,
     SafeAreaView 
 } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
 import { Button } from '../components/Button'
 
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
 export function Confirmation () {
+
+    const navigation = useNavigation()
+
+    function handlePlantSelect() {
+        navigation.navigate('PlantSelect')
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
@@ -24,6 +33,7 @@ export function Confirmation () {
                 <View style={styles.footer}>
                     <Button
                         title="Começar"
+                        onPress={handlePlantSelect}
                     />
                 </View>
 
---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { 
+    StyleSheet, 
+    Text, 
+    View 
+} from 'react-native'
+
+import { Header } from '../components/Header'
+
+import colors from '../styles/colors'
+
+export function PlantSelect(){
+    return (
+        <View style={styles.container}>
+            
+            <Header />
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.background
+    }
+})
